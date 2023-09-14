8551a8eb61945f849c4767779282d26d5671f84f
Tom Hall <thall-tom@hotmail.co.uk>
2023-09-14 13:43:07 +0100

    Make alias aware dict fully implement MutableMapping interface
    Now able to use all dict methods such as get, pop etc

--- src/aliases/dict.py
+++ src/aliases/dict.py
@@ -1,11 +1,14 @@
 import typing as t
+from collections.abc import MutableMapping
 
 if t.TYPE_CHECKING:
     from aliases.space import AliasSpace
     from aliases.string import AliasAwareString
 
+AliasKey: t.TypeAlias = t.Union[str, "AliasAwareString"]
 
-class AliasAwareDict:
+
+class AliasAwareDict(MutableMapping):
     def __init__(
         self, space: "AliasSpace", data: dict[str, t.Any] | None = None, **kwargs
     ) -> None:
@@ -19,11 +22,20 @@ class AliasAwareDict:
     def to_dict(self) -> dict[str, t.Any]:
         return {k.representative: v for k, v in self._data.items()}
 
-    def __getitem__(self, item: t.Union[str, "AliasAwareString"]) -> t.Any:
+    def __getitem__(self, item: AliasKey) -> t.Any:
         return self._data.__getitem__(self.space.str(item))
 
-    def __setitem__(self, key: t.Union[str, "AliasAwareString"], value: t.Any) -> None:
+    def __setitem__(self, key: AliasKey, value: t.Any) -> None:
         return self._data.__setitem__(self.space.str(key), value)
+
+    def __delitem__(self, key: AliasKey) -> None:
+        return self._data.__delitem__(self.space.str(key))
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
 
     def __repr__(self) -> str:
         return f"AliasAwareDict {self.to_dict()}"
